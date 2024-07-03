acf63deb71336a3aa408ed58d6cc678b71655227
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-03 14:17:18 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1*num2);
+console.log(multiply);
+
+let random = Math.floor(Math.random()*7)+1;
+console.log(random);
+
+let num3 = 24;
+let num4 = 5;
+let mod = (num3%num4);
+console.log(mod);
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+console.log(max);
